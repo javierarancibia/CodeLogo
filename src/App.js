577b0e3c758a4808880bda643569bc8cc8c71a09
javierarancibia <69688629+javierarancibia@users.noreply.github.com>
2022-11-25 16:51:09 +0100

    Hover transitions finished!

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ export default function App() {
   return (
     <div className={styles.main}>
       <div className={styles.container}>
-        <div className={styles.firstSquare}></div>
-        <div className={styles.secondSquare}></div>
+        <div className={styles.firstSquare}>
+          <p className={styles.text1}>C</p>
+        </div>
+        <div className={styles.secondSquare}>
+          <p className={styles.text2}>o</p>
+        </div>
       </div>
       <div className={styles.container}>
-        <div className={styles.thirdSquare}></div>
-        <div className={styles.fourthSquare}></div>
+        <div className={styles.thirdSquare}>
+          <p className={styles.text3}>d</p>
+        </div>
+        <div className={styles.fourthSquare}>
+          <p className={styles.text4}>e</p>
+        </div>
       </div>
       <h5>Hover the squares!</h5>
     </div>
